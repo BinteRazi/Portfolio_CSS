1797d14c5f1c23085c9848b84c7d1ac15112b713
BinteRazi <160911585+BinteRazi@users.noreply.github.com>
2025-01-14 20:51:02 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import Transition from './components/Transition';
-import styles from './page.module.css' ;
+import styles from './page.module.css';
 
 const Homepage = () => {
   return (
-    <main className="homepage">
-      <div className="content">
+    <main className={styles.homepage}>
+      <div className={styles.content}>
         <HeroSection />
         <Transition />
       </div>
